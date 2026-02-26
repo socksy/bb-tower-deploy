fc52891ea616066d1d12617c1caabdfd9cad16b1
Ben Lovell <ben.j.lovell@gmail.com>
2026-02-26 16:06:31 +0100

    Move from prepackaging babashka to downloading it on load - actually much faster than i realized, and no longer hit the new 50MB upload limit

--- src/bb_tower_deploy/core.clj
+++ src/bb_tower_deploy/core.clj
@@ -6,14 +6,15 @@
             [clojure.java.io :as io]))
 
 (defn render-template [template-content substitutions]
-  "Render a template with given substitutions"
   (reduce (fn [content [placeholder value]]
             (str/replace content placeholder value))
           template-content
           substitutions))
 
+(defn fetch-latest-bb-version []
+  (str/trim (:body (http/get "https://raw.githubusercontent.com/babashka/babashka/refs/heads/master/resources/BABASHKA_RELEASED_VERSION"))))
+
 (defn create-towerfile [{:keys [app-name default-task default-task-args]}]
-  "Creates a Towerfile with some sensible defaults for babashka projects"
   (when-not (fs/exists? "Towerfile")
     (println "Creating Towerfile...")
     (spit "Towerfile"
@@ -23,73 +24,22 @@
                            "{{DEFAULT_TASK_ARGS}}" (or default-task-args "")}))
     (println "Towerfile created")))
 
-(defn create-python-wrapper []
-  "Creates the Python wrapper script that calls babashka"
+(defn create-python-wrapper [bb-version]
   (when-not (fs/exists? "bb_wrapper.py")
     (println "Creating Python wrapper (bb_wrapper.py)...")
-    (spit "bb_wrapper.py" (slurp (io/resource "bb_wrapper.py")))
+    (spit "bb_wrapper.py"
+          (render-template (slurp (io/resource "bb_wrapper.py"))
+                          {"{{BB_VERSION}}" bb-version}))
     (shell "chmod +x bb_wrapper.py")
     (println "bb_wrapper.py created")))
 
-(defn fetch-babashka-binaries [version]
-  "Downloads babashka binaries for Linux and macOS (AMD64 and ARM64)"
-  (println (str "Fetching babashka binaries (version: " version ")..."))
-  (fs/create-dirs "bin")
-
-  (let [actual-version (if (= version "latest")
-                         (str/trim (:body (http/get "https://raw.githubusercontent.com/babashka/babashka/refs/heads/master/resources/BABASHKA_RELEASED_VERSION")))
-                         version)]
-    (println "Downloading Linux AMD64 binary...")
-    (with-open [stream (:body (http/get (str "https://github.com/babashka/babashka/releases/download/v" actual-version "/babashka-" actual-version "-linux-amd64-static.tar.gz") {:as :stream}))]
-      (shell {:in stream :dir "bin"} "tar -xz bb"))
-    (when (fs/exists? "bin/bb-linux-amd64")
-      (fs/delete "bin/bb-linux-amd64"))
-    (fs/move "bin/bb" "bin/bb-linux-amd64")
-
-    (println "Downloading Linux ARM64 binary...")
-    (with-open [stream (:body (http/get (str "https://github.com/babashka/babashka/releases/download/v" actual-version "/babashka-" actual-version "-linux-aarch64-static.tar.gz") {:as :stream}))]
-      (shell {:in stream :dir "bin"} "tar -xz bb"))
-    (when (fs/exists? "bin/bb-linux-aarch64")
-      (fs/delete "bin/bb-linux-aarch64"))
-    (fs/move "bin/bb" "bin/bb-linux-aarch64")
-
-    (println "Downloading macOS AMD64 binary...")
-    (with-open [stream (:body (http/get (str "https://github.com/babashka/babashka/releases/download/v" actual-version "/babashka-" actual-version "-macos-amd64.tar.gz") {:as :stream}))]
-      (shell {:in stream :dir "bin"} "tar -xz bb"))
-    (when (fs/exists? "bin/bb-macos-amd64")
-      (fs/delete "bin/bb-macos-amd64"))
-    (fs/move "bin/bb" "bin/bb-macos-amd64")
-
-    (println "Downloading macOS ARM64 binary...")
-    (with-open [stream (:body (http/get (str "https://github.com/babashka/babashka/releases/download/v" actual-version "/babashka-" actual-version "-macos-aarch64.tar.gz") {:as :stream}))]
-      (shell {:in stream :dir "bin"} "tar -xz bb"))
-    (when (fs/exists? "bin/bb-macos-aarch64")
-      (fs/delete "bin/bb-macos-aarch64"))
-    (fs/move "bin/bb" "bin/bb-macos-aarch64"))
-
-  (println "Babashka binaries downloaded successfully"))
-
-(defn update-gitignore []
-  "Adds babashka binaries to .gitignore"
-  (when (fs/exists? ".gitignore")
-    (let [current-content (slurp ".gitignore")
-          entries ["bin/bb-linux-amd64" "bin/bb-linux-aarch64" "bin/bb-macos-amd64" "bin/bb-macos-aarch64"]
-          missing-entries (filter #(not (str/includes? current-content %)) entries)]
-      (when (seq missing-entries)
-        (println "Updating .gitignore...")
-        (spit ".gitignore"
-              (str current-content
-                   (when-not (str/ends-with? current-content "\n") "\n")
-                   (str/join "\n" missing-entries) "\n"))
-        (println ".gitignore updated")))))
-
 (defn setup [{:keys [babashka-version] :as opts}]
-  "Sets up everything needed for Tower deployment"
   (println "Setting up Tower deployment for babashka project...")
-  (create-towerfile opts)
-  (create-python-wrapper)
-  (fetch-babashka-binaries (or babashka-version "latest"))
-  (update-gitignore)
+  (let [bb-version (or babashka-version (do (println "Fetching latest babashka version...")
+                                            (fetch-latest-bb-version)))]
+    (println (str "Using babashka v" bb-version))
+    (create-towerfile opts)
+    (create-python-wrapper bb-version))
   (println "\nTower deployment setup complete!")
   (println "\nNext steps:")
   (println "1. Customize your Towerfile as needed")
@@ -97,7 +47,6 @@
   (println "3. Deploy with: tower deploy"))
 
 (defn -main [& args]
-  "Main entry point for bbin"
   (let [opts (apply hash-map args)]
     (setup {:app-name (get opts "--app-name")
             :default-task (get opts "--default-task")
